72e60c83a64f9f7c361b43b372f21766706b21a1
grohtash <rohtash.singh@kiwitech.com>
2018-10-11 18:53:05 +0530

    My first commit after login and logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AdminComponent } from './admin/admin.component';
+import { MaterialFormDemoComponent } from './material-form-demo/material-form-demo.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',redirectTo:'home',pathMatch:'full'},
+  { path:'home',component:HomeComponent},
+  { path:'login',component:LoginComponent},
+  { path:'register',component:RegisterComponent},  
+  { path:'forgot-password',component:ForgotPasswordComponent},
+  { path:'admin',component:AdminComponent,canActivate:[AuthGuard]},
+  { path:'m-demo',component:MaterialFormDemoComponent},
+  { path:'**',component:PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AdminComponent } from './admin/admin.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MaterialFormDemoComponent } from './material-form-demo/material-form-demo.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from './services/auth.guard';
+import { AuthService } from './services/auth.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    AdminComponent,
+    MaterialFormDemoComponent,
+    HeaderComponent,
+    RegisterComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,    
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule    
   ],
-  providers: [],
+  providers: [MaterialModule,AuthGuard,AuthService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
